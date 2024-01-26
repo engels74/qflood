4144d208099b4c722873fc1fc1b4594c03518e89
engels74 <141435164+engels74@users.noreply.github.com>
2024-01-27 00:50:13 +0100

    fix: use qBittorrent instead of rtorrent

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -5,12 +5,19 @@ FROM ${UPSTREAM_IMAGE}@${UPSTREAM_DIGEST_ARM64}
 EXPOSE 3000
 ENV FLOOD_AUTH="false" WEBUI_PORTS="3000/tcp,3000/udp"
 
-ARG RTORRENT_VERSION
-RUN curl -fsSL "https://github.com/jesec/rtorrent/releases/download/v${RTORRENT_VERSION}/rtorrent-linux-arm64" > "${APP_DIR}/rtorrent" && \
-    chmod 755 "${APP_DIR}/rtorrent"
+RUN ln -s "${CONFIG_DIR}" "${APP_DIR}/qBittorrent"
+
+RUN apk add --no-cache unzip
+
+ARG QBITTORRENT_FULL_VERSION
+RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${QBITTORRENT_FULL_VERSION}/x86_64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox"
 
 ARG FLOOD_VERSION
-RUN curl -fsSL "https://github.com/jesec/flood/releases/download/v${FLOOD_VERSION}/flood-linux-arm64" > "${APP_DIR}/flood" && \
+RUN curl -fsSL "https://nightly.link/jesec/flood/actions/runs/${FLOOD_VERSION}/flood-linux-x64.zip" > flood.zip && \
+    unzip -q flood.zip -d "${APP_DIR}/" && \
+    rm -f flood.zip && \
+    mv ${APP_DIR}/flood-linux-x64 ${APP_DIR}/flood && \
     chmod 755 "${APP_DIR}/flood"
 
 COPY root/ /
--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-rtorrent_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/rtorrent/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${rtorrent_version} ]] && exit 0
-flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -r .tag_name | sed s/v//g)
+workflow_url="https://api.github.com/repos/jesec/flood/actions/workflows/publish-rolling.yml/runs?status=success&branch=master"
+latest_run_id=$(curl -u "${GITHUBACTOR}:${GITHUBTOKEN}" -fsSL "${workflow_url}" | jq -r '.workflow_runs | .[0].id')
+[[ -z ${latest_run_id} ]] && exit 0
+
+flood_version="${latest_run_id}"
 [[ -z ${flood_version} ]] && exit 0
-version="${rtorrent_version}--${flood_version}"
+
+qbittorrent_full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -r '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"')
+qbittorrent_version=$(echo "${qbittorrent_full_version}" | sed -e "s/release-//g" -e "s/_.*//g")
+[[ -z ${qbittorrent_version} ]] && exit 0
+
+version="${qbittorrent_version}--${flood_version}"
 version_json=$(cat ./VERSION.json)
-jq '.version = "'"${version}"'" | .flood_version = "'"${flood_version}"'" | .rtorrent_version = "'"${rtorrent_version}"'"' <<< "${version_json}" > VERSION.json
+jq '.version = "'"${version}"'" | .qbittorrent_version = "'"${qbittorrent_version}"'" | .qbittorrent_full_version = "'"${qbittorrent_full_version}"'" | .flood_version = "'"${flood_version}"'"' <<< "${version_json}" > VERSION.json
