5b3fcfa3d1e01f83e26a01fc985827ede97b8a32
engels74 <141435164+engels74@users.noreply.github.com>
2024-04-27 12:22:51 +0200

    [skip ci]
    Update update-versions.sh

--- update-versions.sh
+++ update-versions.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 workflow_url="https://api.github.com/repos/jesec/flood/actions/workflows/publish-rolling.yml/runs?status=success&branch=master"
 flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "${workflow_url}" | jq -re '.workflow_runs[0].id') || exit 1
+[[ -z ${flood_version} ]] && exit 0
+[[ ${flood_version} == null ]] && exit 0
 full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
+[[ -z ${full_version} ]] && exit 0
+[[ ${full_version} == null ]] && exit 0
 version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
 combined_version="${version//v/}--${flood_version}"
 json=$(cat VERSION.json)
