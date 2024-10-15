b9046d177f942788ded78554330a7be981bec40b
engels74 <141435164+engels74@users.noreply.github.com>
2024-10-15 14:12:12 +0200

    fix: brain not very iq much [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
+
+# Fetch the full version with the "release-" prefix
+full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
 [[ -z ${full_version} ]] && exit 0
 [[ ${full_version} == null ]] && exit 0
+
+# Fetch the flood version
 flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name) || exit 1
 [[ -z ${flood_version} ]] && exit 0
 [[ ${flood_version} == null ]] && exit 0
-version="${full_version}--${flood_version}"
+
+# Update VERSION.json using jq
 json=$(cat VERSION.json)
 jq --sort-keys \
-    --arg version "${version//v/}" \
     --arg full_version "${full_version}" \
     --arg flood_version "${flood_version//v/}" \
-    '.version = $version | .full_version = $full_version | .flood_version = $flood_version' <<< "${json}" | tee VERSION.json
+    '.version = ($full_version | sub("^release-"; "") + "--" + $flood_version) |
+     .full_version = $full_version |
+     .flood_version = $flood_version' <<< "${json}" | tee VERSION.json
