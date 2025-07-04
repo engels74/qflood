9599e7025ea5cb8ff69bc5496c8e7011b4f94783
engels74 <141435164+engels74@users.noreply.github.com>
2025-07-05 00:58:57 +0200

    feat(libtorrent): Add multi-libtorrent support
    - Add build_revision_lib1 and build_revision_lib2 to VERSION.json
    - Add full_version_lib1 and full_version_lib2 to VERSION.json
    - Update linux-amd64.Dockerfile to download two qbittorrent-nox binaries
    - Update linux-arm64.Dockerfile to download two qbittorrent-nox binaries
    - Modify forwarded_port_update_app to select libtorrent version
    - Modify service-qbittorrent run script to select libtorrent version
    - Update update-versions.sh to fetch and set new libtorrent versions

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -4,15 +4,20 @@ ARG UPSTREAM_DIGEST_ARM64
 FROM ${UPSTREAM_IMAGE}@${UPSTREAM_DIGEST_ARM64}
 EXPOSE 8080 3000
 ARG IMAGE_STATS
-ENV IMAGE_STATS=${IMAGE_STATS} FLOOD_AUTH="false" WEBUI_PORTS="8080/tcp,8080/udp,3000/tcp,3000/udp"
+ENV IMAGE_STATS=${IMAGE_STATS} FLOOD_AUTH="false" WEBUI_PORTS="8080/tcp,8080/udp,3000/tcp,3000/udp" LIBTORRENT="v2"
 
 RUN ln -s "${CONFIG_DIR}" "${APP_DIR}/qBittorrent"
 
 RUN apk add --no-cache unzip
 
-ARG FULL_VERSION
-RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION}/aarch64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox" && \
-    chmod 755 "${APP_DIR}/qbittorrent-nox"
+ARG FULL_VERSION_LIB1
+ARG BUILD_REVISION_LIB1
+ARG FULL_VERSION_LIB2
+ARG BUILD_REVISION_LIB2
+RUN curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION_LIB1}/aarch64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox-lib1" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox-lib1" && \
+    curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${FULL_VERSION_LIB2}/aarch64-qbittorrent-nox" > "${APP_DIR}/qbittorrent-nox-lib2" && \
+    chmod 755 "${APP_DIR}/qbittorrent-nox-lib2"
 
 ARG FLOOD_VERSION
 RUN curl -fsSL "https://nightly.link/jesec/flood/actions/runs/${FLOOD_VERSION}/flood-linux-x64.zip" > flood.zip && \
--- update-versions.sh
+++ update-versions.sh
@@ -3,14 +3,20 @@ workflow_url="https://api.github.com/repos/jesec/flood/actions/workflows/publish
 flood_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "${workflow_url}" | jq -re '.workflow_runs[0].id') || exit 1
 [[ -z ${flood_version} ]] && exit 0
 [[ ${flood_version} == null ]] && exit 0
-full_version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
-[[ -z ${full_version} ]] && exit 0
-[[ ${full_version} == null ]] && exit 0
-version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version}")
+full_version_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"') || exit 1
+build_revision_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib1}/dependency-version.json" | jq -re '.revision') || exit 1
+full_version_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"') || exit 1
+build_revision_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib2}/dependency-version.json" | jq -re '.revision') || exit 1
+version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version_lib2}")
+[[ -z ${version} ]] && exit 0
+[[ ${version} == null ]] && exit 0
 combined_version="${version//v/}--${flood_version}"
 json=$(cat VERSION.json)
 jq --sort-keys \
     --arg version "${combined_version}" \
-    --arg full_version "${full_version}" \
+    --arg full_version_lib1 "${full_version_lib1}" \
+    --arg build_revision_lib1 "${build_revision_lib1}" \
+    --arg full_version_lib2 "${full_version_lib2}" \
+    --arg build_revision_lib2 "${build_revision_lib2}" \
     --arg flood_version "${flood_version}" \
-    '.version = $version | .full_version = $full_version | .flood_version = $flood_version' <<< "${json}" | tee VERSION.json
+    '.version = $version | .full_version_lib1 = $full_version_lib1 | .build_revision_lib1 = $build_revision_lib1 | .full_version_lib2 = $full_version_lib2 | .build_revision_lib2 = $build_revision_lib2 | .flood_version = $flood_version' <<< "${json}" | tee VERSION.json
