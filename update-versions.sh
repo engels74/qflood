d1646177a6b2d436e3f3fa7ab153f933471ef0c0
engels74 <141435164+engels74@users.noreply.github.com>
2026-01-19 23:55:56 +0100

    fix: remove GitHub auth from curl to fix unbound variable error [skip ci]

--- update-versions.sh
+++ update-versions.sh
@@ -2,13 +2,13 @@
 set -exuo pipefail
 
 # Fetch the flood version
-version_flood=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name)
+version_flood=$(curl -fsSL "https://api.github.com/repos/jesec/flood/releases/latest" | jq -re .tag_name)
 
 # Fetch libtorrent v1 and v2 versions
-full_version_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"')
-build_revision_lib1=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib1}/dependency-version.json" | jq -re '.revision')
-full_version_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"')
-build_revision_lib2=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib2}/dependency-version.json" | jq -re '.revision')
+full_version_lib1=$(curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_1_2)"')
+build_revision_lib1=$(curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib1}/dependency-version.json" | jq -re '.revision')
+full_version_lib2=$(curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/latest/download/dependency-version.json" | jq -re '. | "release-\(.qbittorrent)_v\(.libtorrent_2_0)"')
+build_revision_lib2=$(curl -fsSL "https://github.com/userdocs/qbittorrent-nox-static/releases/download/${full_version_lib2}/dependency-version.json" | jq -re '.revision')
 version=$(sed -e "s/release-//g" -e "s/_.*//g" <<< "${full_version_lib2}")
 
 # Update meta.json using jq
